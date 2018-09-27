ca466b9a752d67acd5531610c119186c7a89e277
Fab1711 <fabrice.martinello@gmail.com>
2018-09-27 17:23:31 +0200

    Setup startRemoveExpense

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <header>
       <h1>Expensify</h1>
       <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-      <NavLink to="/create" activeClassName="is-active">Create new expense</NavLink>u
+      <NavLink to="/create" activeClassName="is-active">Create new expense</NavLink>
     </header>
   );
 
